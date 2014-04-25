cce1e1ed9a4abc54b63e735c5669c72e6a67381d
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2014-04-25 12:45:59 +0100

    New name to avoid clash with standard publisher
    Standard publisher does not include ability to run on specific node.

--- jenkjobs/__init__.py
+++ jenkjobs/__init__.py
@@ -1,19 +1,23 @@
-def doxygen(parser, xml_parent, data):
+def rsdt_doxygen(parser, xml_parent, data):
     """yaml: junit
     Publish Doxygen documentation
+
+    Sameas as jenkins
 
     Example::
 
       publishers:
-        - doxygen:
+        - rsdt_doxygen:
             doxyfile: path to doxyfile relative to workspace. Required.
-            working_directory: path to working directory relative to workspace. Defaults to ""
+            folder: path to working directory relative to workspace. Defaults to ""
             node: nodename. Defaults to "".
             keep: whether to keep previous docs. Defaults to false.
     """
     from xml.etree.ElementTree import SubElement
     doxygen = SubElement(xml_parent, 'hudson.plugins.doxygen.DoxygenArchiver')
+    if not data['doxyfile']:
+        raise JenkinsJobsException("The path to a doxyfile must be specified.")
     SubElement(doxygen, 'doxyfilePath').text = data['doxyfile']
     SubElement(doxygen, 'runOnChild').text = data.get("node", "")
-    SubElement(doxygen, 'folderWhereYouRunDoxygen').text = data.get("working_directory", "")
+    SubElement(doxygen, 'folderWhereYouRunDoxygen').text = data.get("folder", "")
     SubElement(doxygen, 'keepAll').text = data.get("keep", "false")
