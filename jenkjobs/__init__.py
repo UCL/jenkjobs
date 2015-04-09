75e06c188d321be5f2721ca7ef58a3888739242b
Mayeul d'Avezac <m.davezac@ucl.ac.uk>
2015-04-09 15:12:57 +0100

    Adding trac plugin

--- jenkjobs/__init__.py
+++ jenkjobs/__init__.py
@@ -9,7 +9,8 @@ def rsdt_doxygen(parser, xml_parent, data):
       publishers:
         - rsdt_doxygen:
             doxyfile: path to doxyfile relative to workspace. Required.
-            folder: path to working directory relative to workspace. Defaults to ""
+            folder: path to working directory relative to workspace.
+            Defaults to ""
             node: nodename. Defaults to "".
             keep: whether to keep previous docs. Defaults to false.
     """
@@ -20,5 +21,26 @@ def rsdt_doxygen(parser, xml_parent, data):
         raise JenkinsJobsException("The path to a doxyfile must be specified.")
     SubElement(doxygen, 'doxyfilePath').text = data['doxyfile']
     SubElement(doxygen, 'runOnChild').text = data.get("node", "")
-    SubElement(doxygen, 'folderWhereYouRunDoxygen').text = data.get("folder", "")
+    SubElement(doxygen, 'folderWhereYouRunDoxygen').text \
+        = data.get("folder", "")
     SubElement(doxygen, 'keepAll').text = data.get("keep", "false")
+
+
+def trac(parser, xml_parent, data):
+    """yaml: trac
+    Adds trac property.
+    Requires trac plugin.
+
+    Example::
+
+        properties:
+            - track:
+                url: url to trac
+    """
+
+    from jenkins_jobs.errors import JenkinsJobsException
+    from xml.etree.ElementTree import SubElement
+    trac = SubElement(xml_parent, 'hudson.plugins.trac.TracProjectProperty')
+    if not data['url']:
+        raise JenkinsJobsException("The path to a doxyfile must be specified.")
+    SubElement(trac, 'tracWebsite').text = data['url']
--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 from setuptools import setup, find_packages
 setup(
-    name = "JenkJobs",
-    version = "0.0.2",
-    install_requires = "jenkins-job-builder",
-    entry_points = {
+    name="JenkJobs",
+    version="0.0.2",
+    install_requires="jenkins-job-builder",
+    entry_points={
       'jenkins_jobs.publishers': [
         'rsdt_doxygen=jenkjobs:rsdt_doxygen',
+      ],
+      'jenkins_jobs.properties': [
+        'trac=jenkjobs:trac',
       ]
     },
-    packages = find_packages(),
+    packages=find_packages(),
     author='RSDT@UCL',
     license='Apache License, Version 2.0',
     description="RSDT@UCL's extension to jenkins-job-builder"
